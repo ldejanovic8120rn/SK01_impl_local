8a4d3ebbe0e37681b1ac95eef65339aa96b952fe
Lazar Dejanovic <ldejanovic8120rn@raf.rs>
2021-10-30 22:57:15 +0200

    Implementacija local - GMTA

--- src/main/java/com/storageimpl/LocalStorage.java
+++ src/main/java/com/storageimpl/LocalStorage.java
@@ -1,35 +1,126 @@
 package com.storageimpl;
 
+import com.google.gson.Gson;
 import com.storage.Storage;
 import com.utils.Privilege;
+import com.utils.StorageInfo;
+import org.apache.commons.io.FileUtils;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class LocalStorage extends Storage {
 
     @Override
     public File getConfig(String path) {
-        return null;
+//        ClassLoader cl = getClass().getClassLoader();
+//        return new File(cl.getResource(path + "/config.json").getFile());
+        return FileUtils.getFile(path + "/config.json");
     }
 
     @Override
     public File getUsers(String path) {
-        return null;
+        ClassLoader cl = getClass().getClassLoader();
+        return new File(cl.getResource(path + "/users.json").getFile());
     }
 
     @Override
     public void createStorage(String path, String storageName, String adminName, String adminPsw) {
+        File storageDir = FileUtils.getFile(path + "/" + storageName);
+        if (!storageDir.exists()) {
+            try {
+                FileUtils.forceMkdir(storageDir);
 
+                File configFile = new File(path + "/" + storageName + "/config.json");
+                File usersFile = new File(path + "/" + storageName + "/users.json");
+
+                initConfig(configFile, path + "/" + storageName, adminName);
+                initUsers(usersFile, adminName, adminPsw);
+
+                FileUtils.touch(configFile);
+                FileUtils.touch(usersFile);
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
     }
 
     @Override
     public void editConfig(String path, String maxSize, String maxNumOfFiles, List<String> unsupportedFiles) {
+        Map<String, Object> configMap = new HashMap<>();
+        configMap.put("path", path);
+        configMap.put("admin", StorageInfo.getStorageInfo().getUser().getName());
+        configMap.put("maxSize", (maxSize != null) ? maxSize : StorageInfo.getStorageInfo().getConfig().getMaxSize());
+        configMap.put("maxNumOfFiles", (maxNumOfFiles != null) ? maxNumOfFiles : StorageInfo.getStorageInfo().getConfig().getMaxNumOfFiles());
+        configMap.put("unsupportedFiles", (unsupportedFiles != null) ? unsupportedFiles : StorageInfo.getStorageInfo().getConfig().getUnsupportedFiles());
 
+        try {
+            Writer writer = new FileWriter(path + "/config.json");
+            new Gson().toJson(configMap, writer);
+            writer.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void editUsers(String path, String name, String password, Privilege privilege) {
+        Map<String, Object> usersMap = new HashMap<>();
+        usersMap.put("name", name);
+        usersMap.put("password", password);
+        usersMap.put("privilege", privilege);
 
+        try {
+            Writer writer = new FileWriter(path + "/users.json", true);
+            writer.append(",");
+            new Gson().toJson(usersMap, writer);
+            writer.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void initConfig(File configFile, String path, String adminName) {
+        Map<String, Object> configMap = new HashMap<>();
+        configMap.put("path", path);
+        configMap.put("admin", adminName);
+        configMap.put("maxSize", null);
+        configMap.put("maxNumOfFiles", null);
+        configMap.put("unsupportedFiles", null);
+
+        try {
+            Writer writer = new FileWriter(configFile);
+            new Gson().toJson(configMap, writer);
+            writer.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void initUsers(File usersFile, String adminName, String adminPsw) {
+        Map<String, Object> usersMap = new HashMap<>();
+        usersMap.put("name", adminName);
+        usersMap.put("password", adminPsw);
+        usersMap.put("privilege", Privilege.ADMIN);
+
+        try {
+            Writer writer = new FileWriter(usersFile);
+            new Gson().toJson(usersMap, writer);
+            writer.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ src/main/java/com/storageimpl/Test.java
@@ -0,0 +1,49 @@
+package com.storageimpl;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.utils.Privilege;
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+            try {
+                File configFile = new File("/Users/lazardejanovic/Desktop/MyStorage/config.json");
+                File usersFile = new File("/Users/lazardejanovic/Desktop/MyStorage/users.json");
+
+                Map<String, Object> configMap = new HashMap<>();
+                configMap.put("path", "/Users/lazardejanovic/Desktop/MyStorage");
+                configMap.put("admin", "adminName");
+                configMap.put("maxSize", "");
+                configMap.put("maxNumOfFiles", "");
+                configMap.put("unsupportedFiles", null);
+
+
+                try {
+                    Writer writer = new FileWriter(configFile);
+                    writer.append(",");
+                    new Gson().toJson(configMap, writer);
+                    writer.close();
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                FileUtils.touch(configFile);
+                FileUtils.touch(usersFile);
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+
+    }
+}
