78d26857040e4486c2ac6f63e1801d34f0761d62
Lazar Dejanovic <ldejanovic8120rn@raf.rs>
2021-10-31 01:40:50 +0200

    Implementacija local - GMTA

--- src/main/java/com/storageimpl/LocalOperations.java
+++ src/main/java/com/storageimpl/LocalOperations.java
@@ -2,33 +2,120 @@ package com.storageimpl;
 
 import com.storage.Operations;
 import com.utils.FileMetadata;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.DirectoryFileFilter;
+import org.apache.commons.io.filefilter.FalseFileFilter;
+import org.apache.commons.io.filefilter.HiddenFileFilter;
+import org.apache.commons.io.filefilter.TrueFileFilter;
 
+import java.io.File;
+import java.io.FileFilter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
+// TODO - proveriti za sve metode da li se fajlovi i putanje nalaze u skladistu
 public class LocalOperations extends Operations {
 
     @Override
     public List<FileMetadata> getAllFiles(String path) {
-        return null;
+        List<File> files = (List<File>) FileUtils.listFiles(new File(path), HiddenFileFilter.VISIBLE, FalseFileFilter.FALSE);
+        return getFileMetadata(files);
     }
 
     @Override
     public List<FileMetadata> getAllDirectories(String path) {
-        return null;
+        File directory = new File("/Users/lazardejanovic/Desktop/MyStorage");
+        File[] files = directory.listFiles((FileFilter) DirectoryFileFilter.DIRECTORY);
+        return getFileMetadata(Arrays.asList(files));
     }
 
     @Override
     public List<FileMetadata> getAllFilesRecursive(String path) {
-        return null;
+        List<File> files = (List<File>) FileUtils.listFiles(new File(path), HiddenFileFilter.VISIBLE, TrueFileFilter.TRUE);
+        return getFileMetadata(files);
     }
 
     @Override
-    public void uploadFile() {
+    public void download(String path) {
+        File file = new File(path);
 
+        try {
+            if (file.isDirectory()) {
+                FileUtils.moveDirectoryToDirectory(file, new File("./"), false);
+            }
+            else {
+                FileUtils.moveFileToDirectory(file, new File("./"), false);
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
-    public void moveFile(String fromPath, String toPath, String fileName) {
+    public void uploadFile(String fromPath, String toPath) {
+        File file = new File(fromPath);
 
+        try {
+            if (file.isDirectory()) {
+                FileUtils.moveDirectoryToDirectory(file, new File(toPath), false);
+            }
+            else {
+                FileUtils.moveFileToDirectory(file, new File(toPath), false);
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void moveFile(String fromPath, String toPath) {
+        File file = new File(fromPath);
+
+        try {
+            if (file.isDirectory()) {
+                FileUtils.moveDirectoryToDirectory(file, new File(toPath), false);
+            }
+            else {
+                FileUtils.moveFileToDirectory(file, new File(toPath), false);
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private List<FileMetadata> getFileMetadata(List<File> files) {
+        List<FileMetadata> filesMD = new ArrayList<>();
+
+        for (File file: files) {
+            String name = file.getName();
+            Date cd = getCreationDate(file);
+            Date lmd = new Date(file.lastModified());
+
+            FileMetadata fmd = new FileMetadata(name, cd, lmd);
+            filesMD.add(fmd);
+        }
+
+        return filesMD;
+    }
+
+    private Date getCreationDate(File file) {
+        BasicFileAttributes attr = null;
+        try {
+            attr = Files.readAttributes(file.toPath(), BasicFileAttributes.class);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return new Date(attr != null ? attr.creationTime().toMillis() : 0);
     }
 }
--- src/main/java/com/storageimpl/Testing.java
+++ src/main/java/com/storageimpl/Testing.java
@@ -1,11 +1,21 @@
 package com.storageimpl;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.*;
+
 import java.io.File;
+import java.io.FileFilter;
+import java.util.Arrays;
+import java.util.List;
 
 public class Testing {
 
     public static void main(String[] args) {
-        deleteAll("/Users/lazardejanovic/Desktop");
+        File directory = new File("/Users/lazardejanovic/Desktop/MyStorage");
+
+        List<File> list = Arrays.asList(directory.listFiles((FileFilter) DirectoryFileFilter.DIRECTORY));
+        //List<File> list = (List<File>) FileUtils.listFilesAndDirs(new File("/Users/lazardejanovic/Desktop/MyStorage"), DirectoryFileFilter.DIRECTORY, DirectoryFileFilter.DIRECTORY);
+        System.out.println(list);
     }
 
     public static void deleteAll(String rootPath) {
